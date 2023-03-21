80b74240ceb1bedc451716786f53a441315b4d97
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-21 16:53:11 -0300

    Add Player Jump!

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,11 +10,12 @@ public class Player : Singleton<Player>
     public float speed;
     public float rotSpeed;
     public float gravity = -9.8f;
+    public float jumpForce = 15f;
 
     private float _vSpeed = 0f;
 
 
-
+    private Animator _animator;
     private CharacterController _characterController;
 
 
@@ -26,16 +27,40 @@ public class Player : Singleton<Player>
     public void Init()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
-        var speedVector = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        var inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputAxisVertical * speed;
+
+        if (_characterController.isGrounded)
+        {
+            _vSpeed = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _vSpeed = jumpForce;
+            }
+        }
+       
+
 
         _vSpeed += gravity * Time.deltaTime;
         speedVector.y = _vSpeed;
 
         _characterController.Move(speedVector * Time.deltaTime);
+
+        _animator.SetBool("Run", inputAxisVertical != 0);
+
     }
+
+
+
+
+  
+
 }
+
