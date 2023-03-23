0a3e7c5b2bfdd824768c38b43c82bbcf3f6126e4
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-22 21:36:46 -0300

    Update EnemyBase and Add Collider!

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Enemy
 {
@@ -10,6 +11,11 @@ namespace Enemy
 
         [SerializeField] private float _currentLife;
 
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
+
         private void Awake()
         {
             Init();
@@ -18,7 +24,7 @@ namespace Enemy
         protected virtual void Init()
         {
             ResetLife();
-
+            if (startWithBornAnimation) BornAnimation();
         }
 
         protected void ResetLife()
@@ -45,6 +51,16 @@ namespace Enemy
         {
             Destroy(gameObject);
         }
+
+
+        #region ANIMATIONS
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+
+
+        #endregion
 
 
         // debug
