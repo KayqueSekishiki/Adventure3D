f6aeb4a52dee87dbc1440266f100e0a6e4ab92f4
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-24 21:48:11 -0300

    Created Slime Purple!

--- Assets/Scripts/Boss/BossBase.cs
+++ Assets/Scripts/Boss/BossBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,8 +23,13 @@ namespace Boss
         public float startAnimationDuration = .5f;
         public Ease startAnimationEase = Ease.OutBack;
 
+        [Header("Walk")]
         public float speed = 5f;
         public List<Transform> waypoints;
+
+        [Header("Attack")]
+        public int attackAmount = 5;
+        public float timeBetweenAttacks = .5f;
 
         private void Awake()
         {
@@ -37,21 +43,46 @@ namespace Boss
 
             stateMachine.RegisterStates(BossAction.INIT, new BossStateInit());
             stateMachine.RegisterStates(BossAction.WALK, new BossStateWalk());
+            stateMachine.RegisterStates(BossAction.ATTACK, new BossStateAttack());
         }
 
-        #region
-        public void GoToRandomPoint()
+        #region WALK
+        public void GoToRandomPoint(Action onArrive = null)
         {
-            StartCoroutine(GoToPointCoroutine(waypoints[Random.Range(0, waypoints.Count)]));
+            StartCoroutine(GoToPointCoroutine(waypoints[UnityEngine.Random.Range(0, waypoints.Count)], onArrive));
         }
 
-        IEnumerator GoToPointCoroutine(Transform t)
+        IEnumerator GoToPointCoroutine(Transform t, Action onArrive = null)
         {
             while (Vector3.Distance(transform.position, t.position) > 1f)
             {
                 transform.position = Vector3.MoveTowards(transform.position, t.position, Time.deltaTime * speed);
                 yield return new WaitForEndOfFrame();
             }
+
+            onArrive?.Invoke();
+        }
+
+        #endregion
+
+
+        #region ATTACK  
+        public void StartAttack(Action endCallback = null)
+        {
+            StartCoroutine(StartAttackCoroutine(endCallback));
+        }
+
+        IEnumerator StartAttackCoroutine(Action endCallback)
+        {
+            int attacks = 0;
+            while (attacks < attackAmount)
+            {
+                attacks++;
+                transform.DOScale(1.1f, .1f).SetLoops(2, LoopType.Yoyo);
+                yield return new WaitForSeconds(timeBetweenAttacks);
+            }
+
+            endCallback?.Invoke();
         }
 
         #endregion
@@ -74,6 +105,12 @@ namespace Boss
         public void SwitchWalk()
         {
             SwitchState(BossAction.WALK);
+        }
+
+        [NaughtyAttributes.Button]
+        public void SwitchAttack()
+        {
+            SwitchState(BossAction.ATTACK);
         }
 
         #endregion
