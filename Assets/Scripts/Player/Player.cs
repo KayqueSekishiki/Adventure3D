87ea29868a2da16e65e3ba5374e3b7290790f867
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-26 15:03:10 -0300

    Add feedback on DamagePlayer with Vignette!

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -118,6 +118,7 @@ public class Player : Singleton<Player>//, IDamageable
     public void Damage(HealthBase h)
     {
         flashColors.ForEach(i => i.Flash());
+        EffectsManager.Instance.ChangeVignette();
     }
 
     public void Damage(float damage, Vector3 dir)
