2cf3c5ba8a5df73f1faaac1a77fd36348d8da8bf
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-29 16:30:54 -0300

    Add Cloths Manager

---
+++ Assets/Scripts/Cloth/ClothManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+
+
+namespace Cloth
+{
+    public enum ClothType
+    {
+        SPEED,
+
+    }
+
+    public class ClothManager : Singleton<ClothManager>
+    {
+        public List<ClothSetup> clothSetups;
+
+        public ClothSetup GetSetupByType(ClothType clothType)
+        {
+            return clothSetups.Find(i => i.clothtype == clothType);
+        }
+    }
+
+    [System.Serializable]
+    public class ClothSetup
+    {
+        public ClothType clothtype;
+        public Texture2D texture;
+    }
+
+}
