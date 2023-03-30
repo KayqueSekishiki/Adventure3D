955fceee5d184a053be8e16131208b9f2f00d95d
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-03-30 18:11:05 -0300

    Update SaveManager: Save Level and PlayerName!

--- Assets/Scripts/SaveManager/SaveManager.cs
+++ Assets/Scripts/SaveManager/SaveManager.cs
@@ -2,21 +2,44 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ebac.Core.Singleton;
 
-public class SaveManager : MonoBehaviour
+public class SaveManager : Singleton<SaveManager>
 {
+
+    private SaveSetup _saveSetup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _saveSetup = new();
+        _saveSetup.lastLevel = 2;
+        _saveSetup.playerName = "Kayque";
+    }
+
+
+    #region SAVE
+
     [NaughtyAttributes.Button]
     private void Save()
     {
-        SaveSetup setup = new();
-        setup.lastLevel = 2;
-        setup.playerName = "Kayque";
-
-        string setupToJson = JsonUtility.ToJson(setup, true);
+        string setupToJson = JsonUtility.ToJson(_saveSetup, true);
         Debug.Log(setupToJson);
         SaveFile(setupToJson);
     }
 
+    public void SaveLastLevel(int level)
+    {
+        _saveSetup.lastLevel = level;
+        Save();
+    }
+
+    public void SaveName(string text)
+    {
+        _saveSetup.playerName = text;
+        Save();
+    }
+    #endregion
     private void SaveFile(string json)
     {
         string path = Application.streamingAssetsPath + "/save.txt";
@@ -28,6 +51,20 @@ public class SaveManager : MonoBehaviour
         Debug.Log(path);
 
         File.WriteAllText(path, json);
+    }
+
+
+    // FOR DEBUG
+    [NaughtyAttributes.Button]
+    private void SaveLevelOne()
+    {
+        SaveLastLevel(1);
+    }
+
+    [NaughtyAttributes.Button]
+    private void SaveLevelFive()
+    {
+        SaveLastLevel(5);
     }
 }
 
