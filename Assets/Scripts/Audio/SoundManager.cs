b01359035cf389df6a38d33efdb37b66755ab051
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-04-01 20:47:03 -0300

    Add SoundManager!

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using Ebac.Core.Singleton;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public List<MusicSetup> musicSetups;
+    public List<SFXSetup> sfxSetups;
 
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    public void PlayMusicByType(MusicType musicType)
+    {
+        var music = GetMusicByType(musicType);
+        musicSource.clip = music.audioClip;
+        musicSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public MusicSetup GetMusicByType(MusicType musicType)
     {
+        return musicSetups.Find(i => i.musicType == musicType);
+    }
 
+    public void PlaySFXByType(SFXType sfxType)
+    {
+        var sfx = GetSFXByType(sfxType);
+        musicSource.clip = sfx.audioClip;
+        musicSource.Play();
+    }
+
+    public SFXSetup GetSFXByType(SFXType sfxType)
+    {
+        return sfxSetups.Find(i => i.sfxType == sfxType);
     }
 }
 
@@ -25,8 +44,25 @@ public enum MusicType
     TYPE_03
 }
 
+[System.Serializable]
 public class MusicSetup
 {
     public MusicType musicType;
+    public AudioClip audioClip;
+}
+
+
+public enum SFXType
+{
+    TYPE_01,
+    TYPE_02,
+    TYPE_03
+}
+
+
+[System.Serializable]
+public class SFXSetup
+{
+    public SFXType sfxType;
     public AudioClip audioClip;
 }
